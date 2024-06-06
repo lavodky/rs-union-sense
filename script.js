0e2c0bc5a122a8708d601d22203b3d7fa2c9f82b
lavodky <s.1dn3y@hotmail.com>
2024-06-06 13:36:29 -0300

    forms4

--- script.js
+++ script.js
@@ -6,20 +6,31 @@ form.addEventListener("submit", e => {
   e.preventDefault();
 
   loader.classList.remove('hidden');
+  button.disabled = true;
 
   fetch(form.action, {
     method: "POST",
-    body: new FormData(document.querySelector(".container form")),
-  }).then(
-    response => response.json()
-  ).then((html) => {
-
+    body: new FormData(form),
+    headers: { 'Accept': 'application/json' }
+  }).then(response => {
+    if (response.ok) {
+      loader.classList.add('hidden');
+      button.innerText = "Enviado ✅";
+      setTimeout(() => {
+        window.location.href = '/success.html';
+      }, 2000);
+    } else {
+      response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+          alert(data["errors"].map(error => error["message"]).join(", "));
+        } else {
+          alert("Oops! There was a problem submitting your form");
+        }
+      })
+    }
+  }).catch(error => {
     loader.classList.add('hidden');
-    button.innerText = "Sent ✅"
-    
-    setInterval(() => {
-      location.reload();
-    }, 2000);
-    // alert('Form Submitted Succesfully!')
+    button.disabled = false;
+    alert("Oops! There was a problem submitting your form");
   });
 });
